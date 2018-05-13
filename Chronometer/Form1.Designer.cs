724fe26f5e354b133c97958e00b80f70a1a60d50
chririva <c.riva9@studenti.unibg.it>
2018-05-13 18:25:57 +0200

    Add project files.

---
+++ Chronometer/Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Chronometer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.startstopButton = new System.Windows.Forms.Button();
+            this.lapButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.timeLabel = new System.Windows.Forms.Label();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // startstopButton
+            // 
+            this.startstopButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startstopButton.Location = new System.Drawing.Point(22, 84);
+            this.startstopButton.Name = "startstopButton";
+            this.startstopButton.Size = new System.Drawing.Size(120, 38);
+            this.startstopButton.TabIndex = 0;
+            this.startstopButton.Text = "Start / Stop";
+            this.startstopButton.UseVisualStyleBackColor = true;
+            this.startstopButton.Click += new System.EventHandler(this.startstopButton_Click);
+            // 
+            // lapButton
+            // 
+            this.lapButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lapButton.Location = new System.Drawing.Point(148, 84);
+            this.lapButton.Name = "lapButton";
+            this.lapButton.Size = new System.Drawing.Size(120, 38);
+            this.lapButton.TabIndex = 1;
+            this.lapButton.Text = "Lap";
+            this.lapButton.UseVisualStyleBackColor = true;
+            this.lapButton.Click += new System.EventHandler(this.lapButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.Location = new System.Drawing.Point(274, 84);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(120, 38);
+            this.resetButton.TabIndex = 2;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // timeLabel
+            // 
+            this.timeLabel.AutoSize = true;
+            this.timeLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 28.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.timeLabel.Location = new System.Drawing.Point(71, 12);
+            this.timeLabel.Name = "timeLabel";
+            this.timeLabel.Size = new System.Drawing.Size(279, 55);
+            this.timeLabel.TabIndex = 3;
+            this.timeLabel.Text = "00:00:00:00";
+            // 
+            // timer
+            // 
+            this.timer.Interval = 10;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3});
+            this.listView1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(21, 150);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(373, 250);
+            this.listView1.TabIndex = 4;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Num";
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Time";
+            this.columnHeader2.Width = 123;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Diff";
+            this.columnHeader3.Width = 153;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(419, 426);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.timeLabel);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.lapButton);
+            this.Controls.Add(this.startstopButton);
+            this.Name = "Form1";
+            this.Text = "Chronometer";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startstopButton;
+        private System.Windows.Forms.Button lapButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label timeLabel;
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+    }
+}
+
---
+++ Chronometer/Form1.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Chronometer
+{
+    public partial class Form1 : Form
+    {
+        int seconds, minutes, hours, centseconds;
+        int last = 1, lastseconds = 0, lastminutes = 0, lasthours = 0, lastcentseconds=0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void resetButton_Click(object sender, EventArgs e){
+            timer.Enabled = false;
+            reset(); // reset the time variables
+            label_update(); //update the label
+            for (int i = listView1.Items.Count - 1; i >= 0; i--)
+            {   listView1.Items[i].Remove();    } //remove all the items
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            reset();
+        }
+
+        private void startstopButton_Click(object sender, EventArgs e)
+        {
+            timer.Enabled=!timer.Enabled;
+        }
+
+        private void lapButton_Click(object sender, EventArgs e){
+            if (timer.Enabled){
+                ListViewItem list = new ListViewItem(last.ToString());
+                list.SubItems.Add(timeLabel.Text);
+                list.SubItems.Add(Time_difference());
+                last++;
+                lastcentseconds = centseconds;
+                lasthours = hours;
+                lastminutes = minutes;
+                lastseconds = seconds;
+                listView1.Items.Add(list);
+            }
+        }
+
+        private string Time_difference()
+        {
+            string time = "+";
+            int calcseconds, calcminutes, calchours, calccentseconds;
+            if (centseconds < lastcentseconds)
+            {
+                seconds--;
+                if(seconds<0)
+                {
+                    seconds += 60;
+                    minutes--;
+                    if(minutes<0)
+                    {
+                        minutes += 60;
+                        hours--;
+                    }
+                }
+                calccentseconds = (centseconds+100) - lastcentseconds;
+            }
+            else
+            {
+                calccentseconds = centseconds - lastcentseconds;
+            }
+
+            if (seconds < lastseconds)
+            {
+                minutes--;
+                if (minutes < 0)
+                {
+                    minutes += 60;
+                    hours--;
+                }
+                calcseconds = (seconds+60) - lastseconds;
+            }
+            else
+            {
+                calcseconds = seconds - lastseconds;
+            }
+
+            if (minutes < lastminutes)
+            {
+                hours--;
+                calcminutes = (minutes + 60) - lastminutes;
+            }
+            else
+            {
+                calcminutes = minutes - lastminutes;
+            }
+
+            calchours = hours - lasthours;
+
+            //build the string
+            if (calchours < 10)
+                time = time + "0" + calchours.ToString() + ":";
+            else
+                time = time + calchours.ToString() + ":";
+            if (calcminutes < 10)
+                time = time + "0" + calcminutes.ToString() + ":";
+            else
+                time = time + calcminutes.ToString() + ":";
+            if (calcseconds < 10)
+                time = time + "0" + calcseconds.ToString() + ":";
+            else
+                time = time + calcseconds.ToString() + ":";
+            if (calccentseconds < 10)
+                time = time + "0" + calccentseconds.ToString();
+            else
+                time = time + calccentseconds.ToString();
+
+            return time;
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            centseconds++;
+            if (centseconds >= 100){
+                centseconds = 0;
+                seconds++;
+                if(seconds>=60){
+                    seconds = 0;
+                    minutes++;
+                    if (minutes >= 60){
+                        minutes = 0;
+                        hours = 0;
+                    }
+                }
+            }
+            label_update(); //just create the string to display
+        }
+        void reset()
+        {
+            seconds = 0; minutes = 0; hours = 0; centseconds = 0;
+            last = 1; lastcentseconds = 0; lasthours = 0; lastminutes = 0; lastseconds = 0;
+        }
+        void label_update()
+        {
+            String time = "";
+            if (hours < 10)
+                time = time + "0" + hours.ToString() + ":";
+            else
+                time = time + hours.ToString() + ":";
+            if (minutes < 10)
+                time = time + "0" + minutes.ToString() + ":";
+            else
+                time = time + minutes.ToString() + ":";
+            if (seconds < 10)
+                time = time + "0" + seconds.ToString() + ":";
+            else
+                time = time + seconds.ToString() + ":";
+            if (centseconds < 10)
+                time = time + "0" + centseconds.ToString();
+            else
+                time = time + centseconds.ToString();
+            timeLabel.Text = time;
+        }
+    }
+}
